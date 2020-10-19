63c7f0f0241f0d60753f62a25c6206f7dc16480c
Matheus Foganholo <matheusfoganholo@hotmail.com>
2020-10-18 22:12:33 -0300

    Created new bank project, 'Employees'

---
+++ Employees/Classes/Employees/Employee.vb
@@ -0,0 +1,15 @@
+﻿Public Class Employee
+#Region "Properties/Propriedades"
+    Public Property Name As String
+    Public Property CPF As String
+    Public Property Salary As Double
+#End Region
+#Region "Methods/Métodos"
+    ' Employee Anual Bonification
+    ' Bonificação Anual do Funcionário
+    Public Function GetBonus() As Double
+        Return Salary * 0.1
+    End Function
+#End Region
+
+End Class
